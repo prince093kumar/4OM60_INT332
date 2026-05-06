260dc496700044593eee94939dca4ffc67f480f0
Demo <princekumar09372@gmail.com>
2026-05-06 12:16:56 +0530

    maven_practical_1_2

---
+++ student-app/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven directory structure is correct.");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name;
+    }
+}
